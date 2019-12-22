f85cd24b05e5df6204e02716bf4dcd62bd2c3833
canangok <canangok186@gmail.com>
2019-12-22 23:18:29 +0300

    About Page has been updated, Edit Page added

--- MvcBlogProject.Entities/AboutPage.cs
+++ MvcBlogProject.Entities/AboutPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,9 @@ namespace MvcBlogProject.Entities
     public class AboutPage
     {
         public int Id { get; set; }
+        [Display(Name ="Başlık")]
         public string TitleName { get; set; }
+        [Display(Name = "İçerik")]
         public string AboutDescription { get; set; }
     }
 }
--- MvcBlogProject/Areas/Admin/Controllers/AboutController.cs
+++ MvcBlogProject/Areas/Admin/Controllers/AboutController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MvcBlogProject.Entities;
+using MvcBlogProject.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,32 @@ namespace MvcBlogProject.Areas.Admin.Controllers
 {
     public class AboutController : Controller
     {
+        private AboutServices _aboutServices;
+        public AboutController()
+        {
+            _aboutServices = new AboutServices();
+        }
         // GET: Admin/About
+        [HttpGet]
         public ActionResult Index()
         {
+            var aboutModel = _aboutServices.GetAbout();
+            return View(aboutModel);
+        }
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            var aboutModel = _aboutServices.GetAbout();
+            return View(aboutModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(AboutPage aboutPage)
+        {
+           
             return View();
         }
+
+
     }
 }
